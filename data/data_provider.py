709b6d904486c8dcb1b9cddd13e8e003a4d89574
mahshadlotfinia <mahshadlotfinia66@gmail.com>
2023-10-09 18:41:51 +0200

    new leng and getitem

--- data/data_provider.py
+++ data/data_provider.py
@@ -23,68 +23,73 @@ train_mean = [0.59685254, 0.59685254, 0.59685254]
 train_std = [0.16043035, 0.16043035, 0.16043035]
 epsilon = 1e-15
 
-
 class mydataset(Dataset):
-    def __init__(self, data_input_path, CSV_input_path, valid_split_ration, mode = 'train',
-                 transform = transforms.Compose(transforms.ToTensor()), seed = 42):
-
-        self.data_input_path = data_input_path
-        self.CSV_input_path = CSV_input_path
-        self.transform = transform
-        self.valid_split_ration = valid_split_ration
+    def __init__(self, cfg_path, valid_split_ratio=0.2,
+                 transform=transforms.Compose(transforms.ToTensor()), mode=Mode.TRAIN, seed=42):
+        '''
+        Args:
+            cfg_path (string):
+                Config file path of the experiment
+            split_ratio (float):
+                train-valid splitting
+            mode (enumeration Mode):
+                Nature of operation to be done with the data.
+                Possible inputs are Mode.TRAIN, Mode.VALID, Mode.TEST
+                Default value: Mode.TRAIN
+        '''
+        params = read_config(cfg_path)
+        self.input_data_path = params['input_data_path']
+        self.cfg_path = cfg_path
         self.mode = mode
+        self.transform = transform
 
         self.input_list = []
         self.sum_crack = 0
         self.sum_inactive = 0
 
-        with open(CSV_input_path) as csv_file:
-            reader = csv.DictReader (csv_file, delimiter = ';')
+        with open(params['input_csv_path']) as csv_file:
+            reader = csv.DictReader(csv_file, delimiter=';')
             for row in reader:
                 self.input_list.append(row)
 
-                if valid_split_ration == 0:
-                    self.train_list = self.input_list
-                else:
-                    self.train_list, self.valid_list = train_test_split(
-                        self.input_list, test_size = valid_split_ration, random_state = seed
-                    )
+        if valid_split_ratio == 0:
+            self.train_list = self.input_list
+        else:
+            self.train_list, self.valid_list = train_test_split(
+                self.input_list, test_size=valid_split_ratio, random_state=seed)
 
-                for row in self.train_list:
-                    self.sum_crack += int(row['crack'])
-                    self.sum_inactive += int(row['inactive'])
-
-
-
+        for row in self.train_list:
+            self.sum_crack += int(row['crack'])
+            self.sum_inactive += int(row['inactive'])
 
 
     def __len__(self):
-        if self.input_list == self.train_list:
+        '''Returns length of the dataset'''
+        if self.mode == Mode.TRAIN:
             return len(self.train_list)
-        else:
+        elif self.mode == Mode.VALID:
             return len(self.valid_list)
 
-
-
-
     def __getitem__(self, idx):
-        self.output_list = []
+        '''
+        :return:
+            label: e.g. tensor([1, 0]), first element corresponds to "crack" and second to "inactive"
+        '''
+        if self.mode == Mode.TRAIN:
+            output_list = self.train_list
+        elif self.mode == Mode.VALID:
+            output_list = self.valid_list
 
-        if self.mode == 'train':
-            self.output_list = self.train_list
-        elif self.mode == 'valid':
-            self.output_list = self.valid_list
+        label = np.zeros((2), dtype=int)
+        label[0] = int(output_list[idx]['crack'])
+        label[1] = int(output_list[idx]['inactive'])
 
-        label = np.zeros((2), dtype = int)
-        label[0] = int (self.output_list[idx]['crack'])
-        label[1] = int (self.output_list [idx] ['inactive'])
-
-        image = imread(os.path.join(self.data_input_path), self.output_list[idx]['filename'])
+        # Reads images using files name available in the list
+        image = imread(os.path.join(self.input_data_path, output_list[idx]['filename']))
         image = gray2rgb(image)
         image = self.transform(image)
         label = torch.from_numpy(label)
         return image, label
-
 
     def pos_weight(self):
         W_crack = torch.tensor((len(self.train_list) - self.sum_crack) / (self.sum_crack + epsilon))
